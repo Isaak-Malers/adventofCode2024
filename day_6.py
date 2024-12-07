06e0e9629187dfbf21cfbcd34eae1059aa052584
isaak <isaak.malers@gmail.com>
2024-12-06 20:09:06 -0800

    Day 6 Part 2:
    Resorting to brute force to get it done.  How does schedule induced tech debt infiltrate even my fun projects!!!

--- day_6.py
+++ day_6.py
@@ -1,3 +1,5 @@
+import copy
+
 example = """....#.....
 .........#
 ..........
@@ -17,7 +19,7 @@ def parse_input(input_string: str) -> [[str]]:
     return to_return
 
 
-def walk(map: [[str]]) -> [[str]]:
+def walk(map: [[str]]) -> ([[str]], str):
     # find the guard, if not specified:
     guard_row = None
     guard_column = None
@@ -51,6 +53,8 @@ def walk(map: [[str]]) -> [[str]]:
         if current == ">":
             return "v"
 
+    steps_since_las_new = 0
+    grid_size = len(map) * len(map[0])
     while True:
         row, column = find_next_spot()
 
@@ -60,6 +64,13 @@ def walk(map: [[str]]) -> [[str]]:
         if column < 0 or column >= len(map):
             map[guard_row][guard_column] = "X"
             break
+
+        if map[row][column] == ".":
+            steps_since_las_new = 0
+        else:
+            steps_since_las_new += 1
+            if steps_since_las_new > grid_size:
+                return map, "looping"
 
         # if the new spot is un-occupied, or is just something that's already been visited, move into it.
         if map[row][column] in [".", "X"]:
@@ -74,13 +85,30 @@ def walk(map: [[str]]) -> [[str]]:
             map[guard_row][guard_column] = rotated_dude()
             continue
 
-    return map
+    return map, "exited"
 
 def count_spaces(input_string: str) -> int:
     mapped = parse_input(input_string)
-    simulated = walk(mapped)
+    simulated, exited = walk(mapped)
     count_x = sum(rows.count('X') for rows in simulated)
     return count_x
+
+def count_spots_that_cause_loop(input_string: str) -> int:
+    mapped = parse_input(input_string)
+
+    potential_blocks = 0
+    for row in range(0, len(mapped)):
+        print(f"simulating row: {row} of {len(mapped)}")
+        for col in range(0, len(mapped[row])):
+            temp = copy.deepcopy(mapped)
+            if temp[row][col] in ["."]:
+                temp[row][col] = "#"
+                simulated, exited = walk(temp)
+                if exited == "looping":
+                    potential_blocks += 1
+
+    return potential_blocks
+
 
 def get_input() -> str:
     with open('day_6_puzzle.txt', 'r') as file:
@@ -89,3 +117,6 @@ def get_input() -> str:
 
 print(count_spaces(example))
 print(count_spaces(get_input()))
+print()
+print(count_spots_that_cause_loop(example))
+print(count_spots_that_cause_loop(get_input()))
